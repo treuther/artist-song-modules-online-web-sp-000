bd193347ea3cd43a8b85df8af81289526ca9756f
Tracy Reuther <tracyreuther@gmail.com>
2019-07-08 23:44:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,10 +8,10 @@ class Artist
 
   @@artists = []
 
-  def initialize
-    @@artists << self
-    @songs = []
-  end
+  # def initialize
+  #   @@artists << self
+  #   @songs = []
+  # end
 
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
