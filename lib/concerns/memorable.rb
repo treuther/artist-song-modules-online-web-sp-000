8876dedfca1497e9e7d23c4c31567530d52ed956
Tracy Reuther <tracyreuther@gmail.com>
2019-07-08 23:50:43 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,23 +1,17 @@
 module Memorable
-
   module ClassMethods
-
-    def self.reset_all
+    def reset_all
       self.all.clear
     end
 
-    def self.count
+    def count
       self.all.count
     end
-
   end
 
   module InstanceMethods
-
     def initialize
       self.class.all << self
     end
-
   end
-
 end
