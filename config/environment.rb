33be1d1c8e2e65315b98cee0d188095055582180
Tracy Reuther <tracyreuther@gmail.com>
2019-07-09 00:04:55 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -8,3 +8,6 @@ require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
 require_relative '../lib/concerns/findable'
 require_relative '../lib/concerns/memorable.rb'
+
+require_relative '../lib/artist.rb'
+require_relative '../lib/song.rb'
