c1b0850f2c62ab2fe5a653fb068672fcd4a6b951
Tracy Reuther <tracyreuther@gmail.com>
2019-07-09 00:02:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,10 +5,10 @@ class Song
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
 
+  extend Findable
+
   attr_accessor :name
   attr_reader :artist
-
-  extend Findable
 
   @@songs = []
 
